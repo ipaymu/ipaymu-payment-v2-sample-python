ae72b56b18c453395ff3eae3f6f671f5485b604a
Putu Edi Puspayasa <edi.puspayasa@gmail.com>
2022-10-11 09:34:39 +0800

    update qpi check transaction

--- check-transaction.py
+++ check-transaction.py
@@ -8,7 +8,17 @@ ipaymuVa  = "1179000899" #your iPaymu VA
 ipaymuKey = "QbGcoO0Qds9sQFDmY0MWg1Tq.xtuh1" #your iPaymu API Key
 ipaymuUrl = "https://sandbox.ipaymu.com/api/v2/payment" #production: https://my.ipaymu.com
 body =  {
-            "transactionId":"78174"
+            "product":["Jacket"],
+            "qty":["1"],
+            "price": ["150000"],
+            "amount":"10000",
+            "returnUrl":"https://your-website.com/thank-you-page", #your thank you page url
+            "cancelUrl":"https://your-website.com/cancel-page", #your cancel page url
+            "notifyUrl":"https://your-website.com/callback-url", #your callback url
+            "referenceId":"1234", #your reference id or transaction id
+            "buyerName":"Customer Name", #optional
+            "buyerPhone":"08123456789", #optional
+            "buyerEmail":"buyer@mail.com", #optional
         } 
 
 data_body    = json.dumps(body)
